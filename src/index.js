e57c8d9642225063333aed4bbf95ec7cd8cd8ffe
christylaguardia <christinelaguardia@gmail.com>
2017-08-31 23:51:04 -0700

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import recipeReducer from './components/recipe/recipe.reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  recipeReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
