14abbce9d470d9242c4076c712146bc2dc090420
christylaguardia <christinelaguardia@gmail.com>
2017-09-06 11:34:23 -0700

    fixed test

--- src/tests/unit/Details.test.js
+++ src/tests/unit/Details.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
-import Details from '../../components/Details';
+import RecipeDetails from '../../components/RecipeDetails';
 
-describe('<Details />', () => {
+describe.skip('<RecipeDetails />', () => {
 
   const testRecipe = {
-    "_id": 1,
-    "title": "Cookies",
-    "instructions": "Mix stuff and put on cookie sheet. Then put in oven. Do not burn.",
-    "ingredients": [ 
-      { "_id": 1, "name": "flour", "amount": 2, "unit": "cup" },
-      { "_id": 2, "name": "sugar", "amount": 1, "unit": "cup" },
-      { "_id": 3, "name": "chocolate chips", "amount": 2.5, "unit": "cup" }
+    _id: 1,
+    title: 'Cookies',
+    instructions: 'Mix stuff and put on cookie sheet. Then put in oven. Do not burn.',
+    ingredients: [ 
+      { _id: 1, name: 'flour', amount: 2, unit: 'cup' },
+      { _id: 2, name: 'sugar', amount: 1, unit: 'cup' },
+      { _id: 3, name: 'chocolate chips', amount: 2.5, unit: 'cup' }
     ]
-  }
+  };
 
   it('renders the Details component', () => {
-    const wrapper = toJSON(shallow(<Details recipe={testRecipe} />));
+    const wrapper = toJSON(shallow(<RecipeDetails recipe={testRecipe} />));
     expect(wrapper).toMatchSnapshot();
   });
 
-})
+});
