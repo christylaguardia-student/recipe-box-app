c4300b8210b79413bebdd2d9eca1ddfd6dc4d5f0
christylaguardia <christinelaguardia@gmail.com>
2017-09-05 17:54:53 -0700

    update home image

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './styles/index.css';
-import App from './containers/AppContainer';
+import AppContainer from './containers/AppContainer';
 import registerServiceWorker from './registerServiceWorker';
-// import recipeReducers from './store/recipe.reducers';
+import recipeReducer from './store/recipe.reducer';
 
-// const store = createStore(
-//   recipeReducers,
-//   applyMiddleware(thunk),
-//   window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
+const rootReducer = combineReducers({ recipes: recipeReducer });
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById('root')
-// );
+const store = createStore(
+  rootReducer,
+  {},
+  compose(applyMiddleware(thunk)),
+  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
